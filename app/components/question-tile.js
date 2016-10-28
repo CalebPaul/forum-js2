686f2c18b1b434df53c7ade0ebcd5a6f13813482
Caleb Paul <calebpaul21@gmail.com>
2016-10-28 14:44:57 -0700

    fix error in update question hbs

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  updateQuestionForm: false,
-  actions: {
-    update(question, params) {
-      this.sendAction('update', question, params);
-    },
-    delete(question) {
-      if (confirm('Really delete this question?')) {
-        this.sendAction('destroyQuestion', question);
-      }
-    }
-  }
+  // updateQuestionForm: false,
+  // actions: {
+  //   update(question, params) {
+  //     this.sendAction('update', question, params);
+  //   },
+  //   // delete(question) {
+  //   //   if (confirm('Really delete this question?')) {
+  //   //     this.sendAction('destroyQuestion', question);
+  //   //   }
+  //   // }
+  // }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,21 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+  actions: {
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          console.log(question);
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });

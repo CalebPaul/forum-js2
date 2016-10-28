5756fdd6f9d5c78da6bc005b325f4aa9e2925dea
Caleb Paul <calebpaul21@gmail.com>
2016-10-28 15:38:01 -0700

    update model relationships

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   response: DS.attr(),
   author: DS.attr(),
-  timestamp: DS.attr()
+  timestamp: DS.attr(),
+  question: DS.belongsTo('question', { async: true })
 });
--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   query: DS.attr(),
   notes: DS.attr(),
   author: DS.attr(),
-  timestamp: DS.attr()
+  timestamp: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
